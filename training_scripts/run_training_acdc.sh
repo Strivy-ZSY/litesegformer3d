6d9c516274ae1875a9c9b1df5c4cbf9b4edff932
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-10-10 18:38:05 +0800

    Update run_training_acdc.sh

--- training_scripts/run_training_acdc.sh
+++ training_scripts/run_training_acdc.sh
@@ -11,4 +11,4 @@ export litesegformer3d_raw_data_base="$DATASET_PATH"/litesegformer3d_raw
 python lsf3d/run/run_training.py 3d_fullres litesegformer3d_trainer_acdc 1 0
 
 # inference
-# python lsf3d/run/run_training.py 3d_fullres segformer3d_trainer_acdc 1 0 --valbest --validation_only
+# python lsf3d/run/run_training.py 3d_fullres litesegformer3d_trainer_acdc 1 0 --valbest --validation_only
