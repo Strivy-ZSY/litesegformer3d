15f30a2b539924a4f2e9d831619d8790a06dfe68
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-10-10 18:21:35 +0800

    Update run_evaluation_acdc.sh

--- evaluation_scripts/run_evaluation_acdc.sh
+++ evaluation_scripts/run_evaluation_acdc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 DATASET_PATH=../DATASET_Acdc
-CHECKPOINT_PATH=lsf3d/evaluation/lsf3d_acdc_checkpoint
+CHECKPOINT_PATH=lsf3d/evaluation/litesegformer3d_acdc_checkpoint
 
 export PYTHONPATH=.././
 export RESULTS_FOLDER="$CHECKPOINT_PATH"
-export lsf3d_preprocessed="$DATASET_PATH"/lsf3d_raw/lsf3d_raw_data/Task01_ACDC
-export lsf3d_raw_data_base="$DATASET_PATH"/lsf3d_raw
+export litesegformer3d_preprocessed="$DATASET_PATH"/litesegformer3d_raw/litesegformer3d_raw_data/Task01_ACDC
+export litesegformer3d_raw_data_base="$DATASET_PATH"/litesegformer3d_raw
 
 python lsf3d/run/run_training.py 3d_fullres segformer3d_trainer_acdc 1 0 --valbest --validation_only
