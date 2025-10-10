2b57df27dcd5ac567a859de2f9fd774e5dbad181
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-10-10 18:37:13 +0800

    Update run_training_synapse.sh

--- training_scripts/run_training_synapse.sh
+++ training_scripts/run_training_synapse.sh
@@ -7,8 +7,8 @@ export RESULTS_FOLDER=../output_synapse
 export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
-# PYTHONUNBUFFERED=1 nohup python unetr_pp/run/run_training.py 3d_fullres segformer3d_trainer_synapse 2 0  > synapse_segformer_8category.txt 2>&1 &
+# train
+python unetr_pp/run/run_training.py 3d_fullres segformer3d_trainer_synapse 2 0
 
-# over, test grid_minmax=2.6
-
-PYTHONUNBUFFERED=1 nohup python unetr_pp/run/run_training.py 3d_fullres segformer3d_trainer_synapse 2 0  > inf_syn_liteseg.txt 2>&1 &
+# inference
+# python unetr_pp/run/run_training.py 3d_fullres segformer3d_trainer_synapse 2 0 --valbest --validation_only
