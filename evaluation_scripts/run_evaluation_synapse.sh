049745492029ba59f369d3f11861517edd314ff5
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-10-10 18:18:27 +0800

    Update run_evaluation_synapse.sh

--- evaluation_scripts/run_evaluation_synapse.sh
+++ evaluation_scripts/run_evaluation_synapse.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 DATASET_PATH=../DATASET_Synapse
-CHECKPOINT_PATH=unetr_pp/evaluation/unetr_pp_synapse_checkpoint
+CHECKPOINT_PATH=lsf3d/evaluation/lsf3d_synapse_checkpoint
 
 export PYTHONPATH=.././
 export RESULTS_FOLDER="$CHECKPOINT_PATH"
-export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
-export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+export lsf3d_preprocessed="$DATASET_PATH"/lsf3d_raw/lsf3d_raw_data/Task02_Synapse
+export lsf3d_raw_data_base="$DATASET_PATH"/lsf3d_raw
 
-PYTHONUNBUFFERED=1 nohup python unetr_pp/run/run_training.py 3d_fullres segformer3d_trainer_synapse 2 0 -val > eva_segmebest_synapse_2.6.txt 2>&1 & 
+PYTHONUNBUFFERED=1 nohup python lsf3d/run/run_training.py 3d_fullres segformer3d_trainer_synapse 2 0 -val > eva_segmebest_synapse_2.6.txt 2>&1 & 
