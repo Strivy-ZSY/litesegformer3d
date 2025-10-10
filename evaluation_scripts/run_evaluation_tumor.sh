b3c67f38efd5a966b905dc5ac5d157ab5856daa1
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-10-10 18:23:35 +0800

    Update run_evaluation_tumor.sh

--- evaluation_scripts/run_evaluation_tumor.sh
+++ evaluation_scripts/run_evaluation_tumor.sh
@@ -3,15 +3,14 @@
 DATASET_PATH=../DATASET_Tumor
 
 export PYTHONPATH=.././
-export RESULTS_FOLDER=../unetr_pp/evaluation/unetr_pp_tumor_checkpoint
-export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task03_tumor
-export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+export RESULTS_FOLDER=../lsf3d/evaluation/litesegformer3d_tumor_checkpoint
+export litesegformer3d_preprocessed="$DATASET_PATH"/litesegformer3d_raw/litesegformer3d_raw_data/Task03_tumor
+export litesegformer3d_raw_data_base="$DATASET_PATH"/litesegformer3d_raw
 
 
-# Only for Tumor, it is recommended to train unetr_plus_plus first, and then use the provided checkpoint to evaluate. It might raise issues regarding the pickle files if you evaluated without training
+# Only for Tumor, it is recommended to train litesegformer3d first, and then use the provided checkpoint to evaluate. It might raise issues regarding the pickle files if you evaluated without training
 
-python ../unetr_pp/inference/predict_simple.py -i ../unetr_plus_plus/DATASET_Tumor/unetr_pp_raw/unetr_pp_raw_data/Task003_tumor/imagesTs -o ../unetr_plus_plus/unetr_pp/evaluation/unetr_pp_tumor_checkpoint/inferTs -m 3d_fullres  -t 3 -f 0 -chk model_final_checkpoint -tr unetr_pp_trainer_tumor
+python ../lsf3d/inference/predict_simple.py -i ../litesegformer3d/DATASET_Tumor/litesegformer3d_raw/litesegformer3d_raw_data/Task003_tumor/imagesTs -o ../litesegformer3d/litesegformer3d/evaluation/litesegformer3d_tumor_checkpoint/inferTs -m 3d_fullres  -t 3 -f 0 -chk model_final_checkpoint -tr litesegformer3d_trainer_tumor
 
-
-python ../unetr_pp/inference_tumor.py 0
+python ../lsf3d/inference_tumor.py 0
 
