1eccdb4ea855bc1d3ae1b8b141b5e7330cc835cf
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-10-10 18:28:24 +0800

    Update run_training_tumor.sh

--- training_scripts/run_training_tumor.sh
+++ training_scripts/run_training_tumor.sh
@@ -4,10 +4,7 @@ DATASET_PATH=../DATASET_Tumor
 
 export PYTHONPATH=.././
 export RESULTS_FOLDER=../output_tumor
-export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task03_tumor
-export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+export litesegformer3d_preprocessed="$DATASET_PATH"/litesegformer3d_raw/litesegformer3d_raw_data/Task03_tumor
+export litesegformer3d_raw_data_base="$DATASET_PATH"/litesegformer3d_raw
 
-# PYTHONUNBUFFERED=1 python unetr_pp/run/run_training.py 3d_fullres segformer3d_trainer_tumor 3 0 --valbest --validation_only
-
-
-PYTHONUNBUFFERED=1 nohup python unetr_pp/run/run_training.py 3d_fullres segformer3d_trainer_synapse 2 0  > inf_tum_liteseg.txt --valbest --validation_only 2>&1 &
+python litesegformer3d/run/run_training.py 3d_fullres segformer3d_trainer_tumor 3 0
