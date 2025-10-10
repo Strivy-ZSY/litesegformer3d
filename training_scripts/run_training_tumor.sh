ff83a7a960194290f0dfc237b99f6165aaa11c2c
Strivy <107520243+Strivy-ZSY@users.noreply.github.com>
2025-10-10 18:31:10 +0800

    Update run_training_tumor.sh

--- training_scripts/run_training_tumor.sh
+++ training_scripts/run_training_tumor.sh
@@ -7,4 +7,4 @@ export RESULTS_FOLDER=../output_tumor
 export litesegformer3d_preprocessed="$DATASET_PATH"/litesegformer3d_raw/litesegformer3d_raw_data/Task03_tumor
 export litesegformer3d_raw_data_base="$DATASET_PATH"/litesegformer3d_raw
 
-python litesegformer3d/run/run_training.py 3d_fullres segformer3d_trainer_tumor 3 0
+python lsf3d/run/run_training.py 3d_fullres segformer3d_trainer_tumor 3 0
